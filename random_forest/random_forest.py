7166fb897541f75799bb2457c705e0c8c843c1f5
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-11-27 12:41:16 +0600

    poke: random forest

--- random_forest/random_forest.py
+++ random_forest/random_forest.py
@@ -12,40 +12,41 @@ from sklearn.preprocessing import LabelEncoder
 
 # print(os.getcwd())
 
-data_set = pd.read_csv("Datasets/play_tennis.csv")
+data_set = pd.read_csv("datasets/play_tennis.csv")
 
 features = data_set.drop(['Day', 'PlayTennis'], axis=1)
 label = data_set['PlayTennis']
 
-label_encoders = {}
+feature_encoders = {}
 
 for column in features.columns:
-    le = LabelEncoder()
-    features[column] = le.fit_transform(features[column])
-    label_encoders[column] = le
+    feature_encoder = LabelEncoder()
+    features[column] = feature_encoder.fit_transform(features[column])
+    feature_encoders[column] = feature_encoder
 
-print(features)
-
-le = LabelEncoder()
-label = le.fit_transform(label)
+target_encoder = LabelEncoder()
+label = target_encoder.fit_transform(label)
 
 x_train, x_test, y_train, y_test = train_test_split(features, label, test_size=0.3, random_state=42)
 
 
 model = RandomForestClassifier(
-    n_estimators=5, 
+    n_estimators=20, 
     criterion='gini', 
-    max_depth=2,
+    max_depth=1,
     min_samples_split=5,
     min_samples_leaf=3,
-    max_features=4,
+    max_features=3,
     bootstrap=True,
     verbose=1,
-    max_samples=6)
+    max_samples=10)
 
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 
 accuracy = accuracy_score(y_test, y_pred)
+# print(x_test)
+# print(target_encoder.inverse_transform(y_test))
+# print(target_encoder.inverse_transform(y_pred))
 print(accuracy)
 
