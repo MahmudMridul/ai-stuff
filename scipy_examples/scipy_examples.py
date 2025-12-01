4c7f0e2eeb0aa9c8821cb4918242413bd8050ffb
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-12-01 16:36:37 +0600

    scipy: add hstack example

--- scipy_examples/scipy_examples.py
+++ scipy_examples/scipy_examples.py
@@ -1,13 +1,22 @@
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, hstack
 import numpy as np
+import pandas as pd
 
 divider = "========== ========== ========== ========== =========="
 
-mat_1 = np.array([[1, 2, 3], [4, 3, 2]])
+
+def create_dataframe():
+    data = np.array([[0, 1, 2], [5, 3, 8], [8, 6, 5]])
+    df = pd.DataFrame(data, columns=["feature_1", "feature_2", "feature_3"])
+    return df
+
+
+mat_1 = np.array([[1, 2, 3], [4, 3, 2], [9, 8, 8]])
 mat_2 = np.array([[1, 0, 0], [0, 3, 0]])
 
 mat_1_sparsed = csr_matrix(mat_1)
 mat_2_sparsed = csr_matrix(mat_2)
+print(mat_1_sparsed)
 
 # print(mat_1_sparsed)
 # print(divider)
@@ -16,3 +25,14 @@ mat_2_sparsed = csr_matrix(mat_2)
 csr_matrix stores only non zeros
 using csr_matrix is useful when there are many 0's in data. 
 """
+df = create_dataframe()
+
+print(divider)
+print(df)
+
+df_sparsed = csr_matrix(df)
+
+print(divider)
+# scipy hstack only works with sparsed matrix
+combine = hstack([mat_1_sparsed, df_sparsed])
+print(combine)
